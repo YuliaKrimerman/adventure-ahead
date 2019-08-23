653e2224ff7c6d01a359b6e4426cf10fe99cc1e5
YuliaKrimerman <46899367+YuliaKrimerman@users.noreply.github.com>
2019-08-23 17:03:43 -0400

    Deploy

--- src/index.js
+++ src/index.js
@@ -2,9 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import App from './App/App';
+import { usePromiseTracker } from "react-promise-tracker";
+import Loader from 'react-loader-spinner';
 
-
+ const LoadingIndicator = props => {
+	 const { promiseInProgress } = usePromiseTracker();
+   return (
+	   promiseInProgress && 
+     <div
+      style={{
+        width: "100%",
+        height: "100",
+	  	position:"fixed",
+	   	top:"50%",
+	   	left:"50%",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <Loader type="TailSpin" color="#94C5D6" height="100" width="100" />
+    </div>
+  );  
+ }
+ 
 ReactDOM.render(
 	<BrowserRouter>
+	<LoadingIndicator/>
      <App />
+	
    </BrowserRouter>, document.getElementById('root'));
